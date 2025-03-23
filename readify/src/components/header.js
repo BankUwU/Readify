59e7ed6ae2e0bdb0c6a48bb9b82aa4325330818f
BankUwU <bankwasd123@gmail.com>
2025-03-23 23:27:25 +0700

    Sidebar now works properlyy

--- readify/src/components/header.js
+++ readify/src/components/header.js
@@ -1,32 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 
 const Header = ({ username = "User0909" }) => {
-  const handleMenuClick = () => {
-    alert("Menu clicked! You can implement sidebar or dropdown.");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
-    <header className="custom-header">
-      {/* Hamburger Menu */}
-      <div className="menu-icon" onClick={handleMenuClick}>
-        <span></span>
-        <span></span>
-        <span></span>
+    <>
+      <header className="custom-header">
+        {/* Hamburger Menu */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Username & Avatar */}
+        <div className="user-info">
+          <span className="username">{username}</span>
+          <div className="avatar">
+            <img
+              src="https://img.icons8.com/ios-glyphs/30/ffffff/user--v1.png"
+              alt="User Icon"
+            />
+          </div>
+        </div>
+      </header>
+
+      {/* Side Panel */}
+      <div className={`side-panel ${isOpen ? "open" : ""}`}>
+        <button className="close-btn" onClick={toggleMenu}>✖</button>
+        <h3>Menu</h3>
+        <ul>
+          <li><a href="/dashboard">Dashboard</a></li>
+          <li><a href="/profile">Profile</a></li>
+          <li><a href="/settings">Settings</a></li>
+          <li><a href="/logout">Logout</a></li>
+        </ul>
       </div>
 
-      {/* Username & Avatar */}
-      <div className="user-info">
-        <span className="username">{username}</span>
-        <div className="avatar">
-          <img
-            src="https://img.icons8.com/ios-glyphs/30/ffffff/user--v1.png"
-            alt="User Icon"
-          />
-        </div>
-      </div>
-    </header>
+      {/* Overlay to close side panel */}
+      {isOpen && <div className="overlay" onClick={toggleMenu}></div>}
+    </>
   );
 };
 
 export default Header;
+
