6ac74835f1c99295e69feaf33f896d62561b3185
BankUwU <bankwasd123@gmail.com>
2025-04-01 15:28:10 +0700

    OTP page
    Now can Input OTP value
    Redirect to home page after Press register

--- readify/src/App.js
+++ readify/src/App.js
@@ -1,11 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import './App.css';
+import Favorite from "./routes/favorites";
 import Forget from "./routes/forgetpass";
 import Home from "./routes/home";
 import Login from "./routes/login";
-import Register from "./routes/register";
-import Favorite from "./routes/favorites";
 import Myreviews from "./routes/myreviews";
+import OTP from "./routes/Otp";
+import Register from "./routes/register";
 function App() {
     return (
         <Router>
@@ -16,6 +17,7 @@ function App() {
             <Route path="/register" element={<Register />} />
             <Route path="/favorite" element={<Favorite />} />
             <Route path="/my-reviews" element={<Myreviews />} />
+            <Route path="/register/OTP" element={<OTP />} />
           </Routes>
         </Router>
       );
