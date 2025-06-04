65500bf700c67832e6bfed42150ec6284fe08122
BankUwU <bankwasd123@gmail.com>
2025-06-05 03:50:48 +0700

    make search bar wider

--- readify/src/components/ReviewSearchBar.jsx
+++ readify/src/components/ReviewSearchBar.jsx
@@ -1,14 +1,13 @@
-const ReviewSearchBar = ({ searchQuery, onSearchChange, className }) => {
+const ReviewSearchBar = ({ searchQuery, onSearchChange, className = "" }) => {
   return (
     <div className="flex justify-center items-center mt-4 mb-2">
       <input
         type="text"
         placeholder="Search by Book Title or User..."
-        className={`w-full max-w-xs sm:max-w-2xl  border border-gray-300 rounded-md px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-200 ${className}`}
+        className={`border border-gray-300 rounded-md px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-200 ${className}`}
         value={searchQuery}
         onChange={(e) => onSearchChange(e.target.value)}
       />
-      
     </div>
   );
 };
