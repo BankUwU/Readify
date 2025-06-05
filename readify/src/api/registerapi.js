f61cf76cd9a4b58be0d2793c6631357cdeedcc21
BankUwU <bankwasd123@gmail.com>
2025-06-05 13:31:08 +0700

    update default picture

--- readify/src/api/registerapi.js
+++ readify/src/api/registerapi.js
@@ -4,19 +4,23 @@ import { auth, db } from "../config/firebaseConfig"; // Make sure db is imported
 
 export const registerUser = async (email, username, password) => {
   try {
-
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     const user = userCredential.user;
 
-    await updateProfile(user, { displayName: username });
+    // Update both displayName and photoURL for Firebase Authentication user
+    await updateProfile(user, { 
+      displayName: username,
+      photoURL: "https://res.cloudinary.com/djxipn8kj/image/upload/v1749103684/a9wmneb0ad0kfwd3kaqr.png"
+    });
 
+    // Save user data in Firestore 'users' collection
     await setDoc(doc(db, "users", user.uid), {
       username,
       email,
       displayName: username,
-      photoURL: "",
+      photoURL: "https://res.cloudinary.com/djxipn8kj/image/upload/v1749103684/a9wmneb0ad0kfwd3kaqr.png",
       createdAt: serverTimestamp(),
-      role: "user", 
+      role: "user",
     });
 
     return { success: true, user };
